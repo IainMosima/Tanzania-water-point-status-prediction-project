c2ac9d06d852ac5fb4d10654af7ab0e519f1d107
Iain419 <mosimaiain@gmail.com>
2022-07-28 23:30:59 +0300

    Did some optimization in the Logistic Regression Model

--- notebooks/myfunctions.py
+++ notebooks/myfunctions.py
@@ -8,3 +8,33 @@ def set_importer(path, y=False):
         return set.squeeze()
 
     return set
+
+#  function to help with printing score
+def scores(y_true, y_preds, probs=None, print_results=True):
+    from sklearn.metrics import precision_score, recall_score, accuracy_score, auc, roc_curve, log_loss
+    p_s = precision_score(y_true, y_preds)
+    r_s = recall_score(y_true, y_preds)
+    a_s = accuracy_score(y_true, y_preds)
+
+    if print_results:
+      print('The precision score is:\t', p_s)
+      print('The recall score is:\t', r_s)
+      print('The accuracy score is:\t', a_s)
+   
+    if probs.any():
+        ll = log_loss(y_true, probs)
+        fpr, tpr, thr =  roc_curve(y_true, probs)
+        a_c = auc(fpr, tpr)
+        
+        if print_results:
+            print('The log loss is:\t', ll)
+            print('The auc is:\t', a_c)
+    
+
+    
+    if print_results == False:
+        return p_s, r_s, a_s, ll, a_c
+
+        
+
+
